ba73227c0d7d2851af772a9f997e7c2cb0009c30
leosernat <leoserna@hotmail.com>
2025-10-17 09:13:52 -0500

    Add files via upload

---
+++ src/streams/StreamOperation.java
@@ -0,0 +1,11 @@
+package streams;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class StreamOperation {
+    public static void main(String[] args) {
+        }
+}
